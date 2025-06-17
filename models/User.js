eafa51f5ff1aee30d67a1a98c904215949429d20
uFNet1 <91597674+uFNet1@users.noreply.github.com>
2025-06-17 17:03:24 +0300

    add admin logic

--- models/User.js
+++ models/User.js
@@ -1,8 +1,9 @@
 import { DataTypes } from "sequelize";
 import sequelize from "./db.js";
 
-export default sequelize.define('User', {
+export default sequelize.define("User", {
   username: { type: DataTypes.STRING, required: true, unique: true },
   email: { type: DataTypes.STRING, required: true, unique: true },
   password: { type: DataTypes.STRING, required: true },
+  role: { type: DataTypes.STRING, required: true, defaultValue: "user" },
 });
--- routes/auth.js
+++ routes/auth.js
@@ -36,40 +36,53 @@ router.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const user = await User.findOne({ where: {email: email} });
+    const user = await User.findOne({ where: { email: email } });
     if (!user) return res.status(400).json({ msg: "User not found" });
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
 
-    const payload = { id: user.id, username: user.username };
+    const payload = { id: user.id, username: user.username, role: user.role };
 
-    const token = jwt.sign(
-      payload,
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" }
-    );
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
     res.cookie("token", token, {
-          httpOnly: false,
-          secure: false
-        });
-    return res.redirect('/');
+      httpOnly: false,
+      secure: false,
+    });
+    return res.redirect("/");
   } catch (err) {
     res.status(500).json({ msg: "Server error" });
   }
 });
-router.get('/secure', (req, res, next) =>{
+router.get("/secure", (req, res) => {
   const token = req.cookies.token;
   try {
     const user = jwt.verify(token, process.env.JWT_SECRET);
     req.user = user;
-    return res.sendFile(path.join(__dirname, '../public/secure.html'));
+    console.log(user);
+    return res.sendFile(path.join(__dirname, "../public/secure.html"));
   } catch (err) {
-    res.clearCookie('token');
-    return res.redirect('/');
+    res.clearCookie("token");
+    return res.redirect("/");
   }
-  
-})
+});
+router.get("/admin", (req, res) => {
+  const token = req.cookies.token;
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
+    if (user.role === "admin") {
+      return res.sendFile(path.join(__dirname, "../public/admin.html"));
+    } else {
+      return res.redirect("/");
+    }
+  } catch (err) {
+    res.clearCookie("token");
+    return res.redirect("/");
+  }
+});
 // Protected Route
 router.get(
   "/profile",
